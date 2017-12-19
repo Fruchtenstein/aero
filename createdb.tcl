672028fff4c7a90a642ed21166c2e424be727343
Dmitrii Minaev <dmitrii_minaev@epam.com>
2017-12-19 17:15:10 +0400

    2017-12-19: teams distribution

--- createdb.tcl
+++ createdb.tcl
@@ -4,12 +4,12 @@ package require sqlite3
 
 sqlite3 db aerobia.db
 db eval {CREATE TABLE teams (teamid INTEGER PRIMARY KEY, teamname)}
-db eval {CREATE TABLE runners (runnerid INTEGER PRIMARY KEY, teamid INTEGER, goal REAL)}
+db eval {CREATE TABLE runners (runnerid INTEGER PRIMARY KEY, runnername, teamid INTEGER, goal REAL)}
 db eval {CREATE TABLE log (runnerid INTEGER, week INTEGER, distance REAL, PRIMARY KEY(runnerid, week))}
-db eval {INSERT INTO teams values (1, "Мурзилки")}
-db eval {INSERT INTO teams values (2, "Веселые картинки")}
-db eval {INSERT INTO teams values (3, "Крокодилы")}
-db eval {INSERT INTO teams values (4, "Ленивые тюлени")}
+db eval {INSERT INTO teams values (1, "раз")}
+db eval {INSERT INTO teams values (2, "два")}
+db eval {INSERT INTO teams values (3, "три")}
+db eval {INSERT INTO teams values (4, "четыре")}
 #db eval {INSERT INTO runners values (19999, 1, 4500.0)}
 #db eval {INSERT INTO runners values (40, 2, 2750.0)}
 #db eval {INSERT INTO runners values (31319, 3, 3500.0)}
--- loadusers.py
+++ loadusers.py
@@ -3,18 +3,30 @@
 import csv
 import sqlite3
 
-db = sqlite3.connect("aerobia.db")
+db = sqlite3.connect('aerobia.db')
 c = db.cursor()
+nofteams = 4
+teams = [[],[],[],[]]
+goals = [0,0,0,0]
 f = open('users.csv')
 reader = csv.reader(f)
-for row in reader:
-    userid = int(row[1])
-    teamid = int(row[4])
-    goal = float(row[3])
-    print(userid)
-    print(teamid)
-    print(goal)
-    c.execute("INSERT OR REPLACE INTO runners VALUES (?, ?, ?)", (userid, teamid, goal))
+for i, row in enumerate(reader):
+    if i in range(0,3):
+        teams[i].append([row[2], row[1], row[3]])
+        goals[i] += float(row[3])
+    else:
+        goesto = goals.index(min(goals))
+        teams[goesto].append([row[2], row[1], row[3]])
+        goals[goesto] += float(row[3])
+
+print("===========")
+for i, t in enumerate(teams):
+    print ("Команда {} (цель: {:.2f}):".format(i+1, goals[i]/52))
+    print ("==========")
+    for r in t:
+        print(r[0], r[1], r[2])
+        c.execute('INSERT OR REPLACE INTO runners  VALUES (?, ?, ?, ?)', (r[1], r[0], i+1, r[2]))
+    print("")
 db.commit()
 db.close()
 f.close()
