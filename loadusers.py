6dc5510ceefb9b6929015ed34b046f2d7ec05618
Dmitrii Minaev <dmitrii_minaev@epam.com>
2017-12-21 14:31:13 +0400

    add statistics template

--- loadusers.py
+++ loadusers.py
@@ -5,19 +5,18 @@ import sqlite3
 
 db = sqlite3.connect('aerobia.db')
 c = db.cursor()
-nofteams = 4
-teams = [[],[],[],[]]
-goals = [0,0,0,0]
+nofteams = 8
+teams=[]
+goals=[]
+for i in range(0,nofteams):
+    teams.append([])
+    goals.append(0)
 f = open('users.csv')
 reader = csv.reader(f)
 for i, row in enumerate(reader):
-    if i in range(0,3):
-        teams[i].append([row[2], row[1], row[3]])
-        goals[i] += float(row[3])
-    else:
-        goesto = goals.index(min(goals))
-        teams[goesto].append([row[2], row[1], row[3]])
-        goals[goesto] += float(row[3])
+   goesto = goals.index(min(goals))
+   teams[goesto].append([row[2], row[1], row[3]])
+   goals[goesto] += float(row[3])
 
 print("===========")
 for i, t in enumerate(teams):
@@ -25,7 +24,7 @@ for i, t in enumerate(teams):
     print ("==========")
     for r in t:
         print(r[0], r[1], r[2])
-        c.execute('INSERT OR REPLACE INTO runners  VALUES (?, ?, ?, ?)', (r[1], r[0], i+1, r[2]))
+        c.execute('INSERT OR REPLACE INTO runners  VALUES (?, ?, ?, ?, 0)', (r[1], r[0], i+1, r[2]))
     print("")
 db.commit()
 db.close()
--- table.py
+++ table.py
@@ -31,24 +31,6 @@ for t in teams:
     print(".... avg pct: ", tpercentage)
     tbl.append([t[0], t[1], tgoal, tmileage, tmileage*100/tgoal, tpercentage])
 
-#tbl = sorted(tbl, key=lambda x: x[4], reverse = True)
-#output = []
-#output.append('            <center>')
-#output.append('                <h1>Результаты {} недели</h1>'.format(week))
-#output.append('            </center>')
-#
-#output.append('            <div class="datagrid"><table>')
-#output.append('               <thead><tr><th>Команда</th><th>Цель (км/нед)</th><th>Результат (км)</th><th>Выполнено %</th><th>Очки</th></tr></thead>')
-#output.append('               <tbody>')
-#odd = True
-#for n, t in enumerate(tbl):
-#    alt = ' class="alt"' if odd else ''
-#    pts = len(teams)*5-n*5-5
-#    output.append('                 <tr{}><td>{}</td><td>{:.2f}</td><td>{:.2f}</td><td>{:.2f}</td><td>{}</td></tr>'.format(alt, t[1], t[2], t[3], t[4], pts))
-#    c1.execute('INSERT OR REPLACE INTO tlog VALUES (?, ?, ?, ?)', (t[0], week, pts, t[4]))
-#    odd = not odd
-#output.append('               </tbody>')
-#output.append('            </table></div>')
 tbl = sorted(tbl, key=lambda x: x[5], reverse = True)
 output = []
 output.append('            <center>')
@@ -70,27 +52,6 @@ output.append('            </table></div>')
 
 db.commit()
 
-#output2 = []
-#output2.append('            <center>')
-#output2.append('                <h1>Таблица соревнования на {} неделю</h1>'.format(week))
-#output2.append('            </center>')
-#
-#output2.append('            <div class="datagrid"><table>')
-#output2.append('               <thead><tr><th>Команда</th><th>Очки</th></tr></thead>')
-#output2.append('               <tbody>')
-#odd = True
-#tbl=[]
-#for t in teams:
-#    pts = c1.execute('SELECT SUM(pts) FROM tlog WHERE teamid=?',(t[0],)).fetchone()[0]
-#    tbl.append([t[1], pts])
-#
-#tbl = sorted(tbl, key=lambda x: x[1], reverse = True)
-#for n, t in enumerate(tbl):
-#    alt = ' class="alt"' if odd else ''
-#    output2.append('                 <tr{}><td>{}</td><td>{}</td></tr>'.format(alt, t[0], t[1]))
-#    odd = not odd
-#output2.append('               </tbody>')
-#output2.append('            </table></div>')
 output2 = []
 output2.append('            <center>')
 output2.append('                <h1>Таблица соревнования на {} неделю</h1>'.format(week))
@@ -126,8 +87,6 @@ out.write(result)
 out.close()
  
 
-
-
 outteam = []
 outteam.append('            <br />')
 outteam.append('            <center>')
@@ -140,14 +99,12 @@ for t in teams:
     outteam.append('            <div class="datagrid"><table>')
     outteam.append('               <thead><tr><th>Имя</th><th>Цель (км/нед)</th><th>Результат (км)</th><th>Выполнено (%)</th></tr></thead>')
     outteam.append('               <tbody>')
-
-
     runners = c1.execute('SELECT * FROM runners WHERE teamid=?', (t[0],)).fetchall()
     odd = True
     for r in runners:
         alt = ' class="alt"' if odd else ''
         if r[4]==0:
-            rmileage = c1.execute('SELECT sum(distance) FROM log WHERE runnerid=? AND week=?', (r[0], week)).fetchone()[0]
+            rmileage = c1.execute('SELECT distance FROM log WHERE runnerid=? AND week=?', (r[0], week)).fetchone()[0]
             rgoal = r[3]/52
         else:
             rmileage = 0
@@ -158,8 +115,6 @@ for t in teams:
     outteam.append('            </table></div>')
     outteam.append('            <br />')
 
-
-
 inp = open('teams.template')
 tpl = Template(inp.read())
 outstr = '\n'.join(outteam)
@@ -167,6 +122,24 @@ subst = {'table':outstr}
 result = tpl.substitute(subst)
 inp.close()
 out = open('html/teams.html', 'w')
+out.write(result)
+out.close()
+
+outstat = []
+outstat.append('            <br />')
+outstat.append('            <center>')
+outstat.append('                <h1>Лучший бегун {} недели</h1>:'.format(week))
+outstat.append('                <h1>Dimitri ☮ Fruchtenstein</h1>'.format(week))
+outstat.append('            </center>')
+outstat.append('')
+
+inp = open('statistics.template')
+tpl = Template(inp.read())
+outstr = '\n'.join(outstat)
+subst = {'data':outstr}
+result = tpl.substitute(subst)
+inp.close()
+out = open('html/statistics.html', 'w')
 out.write(result)
 out.close()
 
