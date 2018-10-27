18f05a90db935b1a14a1ebbecc7a3ce223323fdb
Dmitrii Minaev <dminaev@ooyala.com>
2018-10-28 00:20:53 +0400

    add config file

---
+++ config.py
@@ -0,0 +1,7 @@
+import datetime
+STARTCHM = datetime.date(2018, 1, 1)
+LASTCHM  = datetime.date(2018, 11, 12)
+ENDCHM   = datetime.date(2018, 11, 14)
+STARTCUP = datetime.date(2018, 11, 19)
+LASTCUP  = datetime.date(2018, 12, 31)
+ENDCUP    = datetime.date(2019, 1, 2)
